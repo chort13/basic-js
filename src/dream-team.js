5c65582ada41e775650ff201fc876c6cded88cdd
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-04-17 21:57:16 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+/*function createDreamTeam( members ) {
+  //throw new NotImplementedError('Not implemented');
+  var newArr='';
+  var Arr='';
+  for(var i=0;i<members.length;i++){
+    if(typeof members[i]==="string"){
+      newArr+=members[i];
+      Arr+=members[i].substr(1, 1)
+    }
+  }
+
+}*/
+
+
+function sortAndReverse(string, reverse = false) {
+  const compare = (new Intl.Collator('en')).compare
+return string.split('')
+  .sort((a, b) => ((reverse ? -1 : 1) * compare(a, b)))
+  .join('')
+}
+
+
+function createDreamTeam( members ) {
+//throw new NotImplementedError('Not implemented');
+
+//if(typeof members =="string")return false
+
+var newArr='';
+var Arr='';
+for(var i=0;i<members.length;i++){
+  if(typeof members[i]==="string"){
+      
+      members[i]=members[i].trim();
+     
+    newArr+=members[i];
+    Arr+=members[i].substr(0, 1)
+   
+  }
+}
+Arr=Arr.toUpperCase();
+Arr=sortAndReverse(Arr)
+
+return Arr
 }
 
 module.exports = {
